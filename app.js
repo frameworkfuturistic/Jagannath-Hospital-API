82f83d50f5f39191ef0fb9d8f1533f6047b8bac1
Prabhu Kumar Saw <54577120+prabhukumarsaw@users.noreply.github.com>
2024-12-11 10:48:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const allowedOrigins = [
   'https://sjhrc.in', // Production domain 1
   'https://appointment.sjhrc.in', // Production domain 2
   'http://localhost:3000', // Local development domain
+  'https://test-hospital-website.vercel.app'
 ];
 
 app.use(cors({
